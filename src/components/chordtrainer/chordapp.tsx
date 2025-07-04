1b9d65d8f5c2cdf7e7200ac5bb70a5f315598a89
Kurt Gregorek <kmgregorek@gmail.com>
2025-07-03 21:48:18 -0400

    Add chord

--- src/components/chordtrainer/chordapp.tsx
+++ src/components/chordtrainer/chordapp.tsx
@@ -1,5 +1,8 @@
 import '../../App.css';
 
+import Chord from '@techies23/react-chords'
+import { useState } from 'react';
+
 function ChordApp () {
 
     const baseStyle = {
@@ -7,11 +10,34 @@ function ChordApp () {
         height: '90vh'
     }
 
+    const chordStyle = {
+        transform: 'scale(0.5)'
+    }
+
+    const [activeChord, setActiveChord] = useState({
+        frets: [1, 3, 3, 2, 1, 1],
+        fingers: [1, 3, 4, 2, 1, 1],
+        barres: [1],
+        capo: false,
+    });
+    const [activeInstrument, setActiveInstrument] = useState({
+        strings: 6,
+        fretsOnChord: 4,
+        name: "Guitar",
+        keys: [],
+        tunings: {
+        standard: ["E", "A", "D", "G", "B", "E"],
+        },
+    })
+
     return <div className='App'>
         <header className="App-header">
         <div className='textblockContainer' style={baseStyle}>
             <a href='/' style={{display: 'inline-block', width: '10%'}}>Back</a>
             <p style={{display: 'inline-block', width: '90%'}}>Coming Soon...</p>
+            <div style={{height: '65%', width: '40%', background: 'white', margin: 'auto'}}>
+                <Chord chord={activeChord} instrument={activeInstrument} lite={true}/>
+            </div>
         </div>
         </header>
     </div>
